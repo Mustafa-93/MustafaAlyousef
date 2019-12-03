e381b03669f9201866544c783e0b78464eee97f7
Mustafa Alyousef <mustafa.integration@gmail.com>
2019-12-03 15:02:53 +0100

    test

--- src/University/Math.java
+++ src/University/Math.java
@@ -1,8 +1,8 @@
 package University;
 
 public class Math {
-    public int sum (int first, int second){
-        return first+second;
+    public int sum (int first, int second,int third){
+        return first+second+third;
     }
     public int sub(int first, int second){
         return first-second;
