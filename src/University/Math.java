19711cff4d1202cb62ff96dc31a532616ed8a5d6
Mustafa Alyousef <mustafa.integration@gmail.com>
2019-12-03 14:04:14 +0100

    Added Sub method to the Math class

--- src/University/Math.java
+++ src/University/Math.java
@@ -4,4 +4,7 @@ public class Math {
     public int sum (int first, int second){
         return first+second;
     }
+    public int sub(int first, int second){
+        return first-second;
+    }
 }
