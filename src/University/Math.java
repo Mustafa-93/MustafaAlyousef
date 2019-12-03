992cb786aa84f3b725f2c4024a018f29801d6b59
Amr Alshaaba <amr_alshaabi@hotmail.com>
2019-12-03 14:48:31 +0100

    test conflict

--- src/University/Math.java
+++ src/University/Math.java
@@ -1,9 +1,15 @@
 package University;
 
 public class Math {
+
+
     public int sum (int first, int second){
-        return first+second;
+
+       int sum = first+second;
+        return sum;
     }
+
+
     public int sub(int first, int second){
         return first-second;
     }
